e2184c223e6991d213fd4ebebe1a22b3369385b8
zhangsanlzh <844804539@qq.com>
2018-03-12 15:03:46 +0800

    新改变

--- GraphElementGenerationSYS/GraphElementGenerationSYS/MainWindow.xaml.cs
+++ GraphElementGenerationSYS/GraphElementGenerationSYS/MainWindow.xaml.cs
@@ -15,8 +15,14 @@ namespace GraphElementGenerationSYS
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();                
         }
+
+        /// <summary>
+        /// 拖动窗体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Window_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -25,27 +31,52 @@ namespace GraphElementGenerationSYS
             }
         }
 
+        /// <summary>
+        /// 窗体关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void FormClose(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
+        /// <summary>
+        /// 窗体最小化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void FormMinimize(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
 
+        /// <summary>
+        /// 显示系统菜单，当点击Icon的时候
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void WindowMenu(object sender, RoutedEventArgs e)
         {
-            SystemCommands.ShowSystemMenu(this, GetMousePosition());
+            SystemCommands.ShowSystemMenu(this, GetMousePosition());//弹出系统菜单            
         }
-
+        
+        /// <summary>
+        /// 设置菜单弹出的位置为鼠标点击的地方
+        /// </summary>
+        /// <returns></returns>
         protected Point GetMousePosition()
         {
             var position = Mouse.GetPosition(this);
             return new Point(position.X + this.Left, position.Y + this.Top);
         }
 
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var w=this.FindResource("MainWindowStyle");
 
+            var win = this.GetTemplateChild("FunctionList");
+            
+        }
     }
 }
